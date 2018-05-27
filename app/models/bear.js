06c29389dad57a8404c7efff4ed4666b5dae3d6c
Rodrigo Dantas <rodrigodc07@gmail.com>
2018-05-27 20:07:31 -0300

    Trabalho BD modularizado

---
+++ app/models/bear.js
@@ -0,0 +1,11 @@
+// app/models/bear.js
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BearSchema   = new Schema({
+    name:{ type:String ,required:true}
+});
+
+module.exports = mongoose.model('Bear', BearSchema,'Ceveja');
+
---
+++ config/server.js
@@ -0,0 +1,25 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var server     = express();                 // define our server using express
+var bodyParser = require('body-parser');
+var connection = require('./database')
+// configure server to use bodyParser()
+// this will let us get the data from a POST
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+var Bear  = require('../app/models/bear');
+
+
+// START THE SERVER
+// =============================================================================
+server.listen(port);
+console.log('Magic happens on port ' + port);
+
